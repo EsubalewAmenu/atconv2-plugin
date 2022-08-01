dd01a62e683e962056f962fb3644d782053c3d95
Esubalew Desktop <esubalew.a2009@gmail.com>
2022-08-01 13:50:45 +0300

    src updated

--- public/partials/sections/hero.php
+++ public/partials/sections/hero.php
@@ -1,21 +1,20 @@
+<?php foreach ($sections as $section) { ?>
 	<section class="theme-slide__wrapper">
 		<app-post-teaser-banner class="background--dark theme-slide" data-scrollama-index="2">
 			<header class="post-teaser-banner">
 				<figure class="post-teaser-banner__crop">
 					<app-loader _nghost-mvrdv-c13="" class="ng-tns-c13-5 ng-star-inserted">
 						<!---->
-					</app-loader><a href="https://mvrdv.nl/themes/9/urbanism">
-					<?php $src = wp_get_attachment_image_src(get_post_thumbnail_id($section->ID), 'thumbnail_size'); ?>
-						<app-image loading="lazy" class="post-teaser-banner__image is--loaded ng-star-inserted" 
-						_nghost-mvrdv-c34="" style="--aspect-ratio: auto"><img _ngcontent-mvrdv-c34="" 
-						alt="Urbanism" src="<?php echo $src ? $src[0] : get_template_directory_uri(); ?>/assets/328_Seoullo7017_2019_%25C2%25A9Ossip_005.jpg" sizes="auto" loading="lazy" />
+					</app-loader><a href="<?php echo esc_url(get_permalink($section->ID)); ?>">
+						<?php $src = wp_get_attachment_image_src(get_post_thumbnail_id($section->ID), 'thumbnail_size'); ?>
+						<app-image loading="lazy" class="post-teaser-banner__image is--loaded ng-star-inserted" _nghost-mvrdv-c34="" style="--aspect-ratio: auto"><img _ngcontent-mvrdv-c34="" alt="Urbanism" src="<?php echo $src[0] ?>" sizes="auto" loading="lazy" />
 							<!---->
 						</app-image>
 						<!---->
 						<!---->
 					</a>
 				</figure>
-				<a class="post-teaser-banner__content" href="https://mvrdv.nl/themes/9/urbanism">
+				<a class="post-teaser-banner__content" href="<?php echo esc_url(get_permalink($section->ID)); ?>">
 					<div class="post-teaser-banner__content__wrapper">
 						<h1 class="post-teaser-banner__content__claim">
 							<span class="post-teaser-banner__content__claim__span ng-star-inserted">smart &amp; </span><span class="post-teaser-banner__content__claim__span ng-star-inserted">liveable cities</span>
@@ -38,13 +37,13 @@
 					<article class="post-teaser">
 						<div class="post-teaser__heading">
 							<h1 class="post-teaser__heading__title">
-								<a href="https://mvrdv.nl/themes/9/urbanism">
+								<a href="<?php echo esc_url(get_permalink($section->ID)); ?>">
 									<span class="ng-star-inserted">124,673,491</span>
 									m² of urban projects
 								</a>
 							</h1>
 						</div>
-						<a class="post-teaser__button ng-star-inserted" href="https://mvrdv.nl/themes/9/urbanism">
+						<a class="post-teaser__button ng-star-inserted" href="<?php echo esc_url(get_permalink($section->ID)); ?>">
 							<app-icon><span class="icon is--icon-arrow"><svg class="icon-svg">
 										<use xlink:href="/login_files/gfx/svg-sprite.svg#icon-arrow"></use>
 									</svg></span></app-icon>
@@ -56,3 +55,4 @@
 			<!---->
 		</ul>
 	</section>
+<?php } ?>
