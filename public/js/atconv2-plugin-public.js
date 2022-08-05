23d6e7a7d21acbf53c7291a20313619b2447fa88
Esubalew Desktop <esubalew.a2009@gmail.com>
2022-08-05 18:28:54 +0300

    title animation added

--- public/js/atconv2-plugin-public.js
+++ public/js/atconv2-plugin-public.js
@@ -1,32 +1,14 @@
-(function( $ ) {
-	'use strict';
-
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
-})( jQuery );
+function isScrolledIntoView(elem) {
+	var docViewTop = jQuery(window).scrollTop();
+	var docViewBottom = docViewTop + jQuery(window).height();
+	var elemTop = jQuery(elem).offset().top;
+	var elemBottom = elemTop + jQuery(elem).height();
+	return elemBottom <= docViewBottom && elemTop >= docViewTop;
+}
+jQuery(window).scroll(function () {
+	jQuery(".post-teaser-banner__content__claim").each(function () {
+		if (isScrolledIntoView(this) === true) {
+			jQuery(this).addClass("reduceSize");
+		}
+	});
+}); 
